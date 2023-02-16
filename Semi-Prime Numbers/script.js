77d6c728fdab9d22a605b535522b0d26200796ba
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2023-02-16 16:29:44 +0200

    Changing sort at semi-prime(not solved)

--- Semi-Prime Numbers/script.js
+++ Semi-Prime Numbers/script.js
@@ -12,15 +12,19 @@ const semiPrimeCount = (n) => {
         }
         if (check && i % 1 === 0) {
           primeNumbers.push(i);
-          /*if (primeNumbers.length > 1) {
             primeNumbers.forEach(number => {
               const semiPrimeNumber = number * i;
-              console.log(semiPrimeNumber);
-              if (semiPrimeNumber < n) semiPrimeNumbers.push(semiPrimeNumber);
+              console.log(semiPrimeNumbers);
+              if (semiPrimeNumber < n) {
+                if (semiPrimeNumber < semiPrimeNumbers[semiPrimeNumbers.length - 1]) {
+                  const temp = semiPrimeNumbers[semiPrimeNumbers.length - 1];
+                  semiPrimeNumbers[semiPrimeNumbers.length - 1] = semiPrimeNumber;
+                  semiPrimeNumbers.push(temp);
+                } else {
+                  semiPrimeNumbers.push(semiPrimeNumber);
+                }
+              }
             });
-          } else {
-            if (i * i < n) semiPrimeNumbers.push(i * i);
-          }*/
         }
         check = true;
         j = 2;
@@ -31,15 +35,16 @@ const semiPrimeCount = (n) => {
         if (squarNumber < n) semiPrimeNumbers.push(squarNumber);
         if (semiPrimeNumber < n) semiPrimeNumbers.push(semiPrimeNumber);
       }*/
-      primeNumbers.forEach((number, index) => {
+      /*primeNumbers.forEach((number, index) => {
         const caseOne = Math.pow(number, 2);
         if (caseOne < n) semiPrimeNumbers.push(caseOne);
         for (let i = index + 1; i < primeNumbers.length - 1; i++) {
             const caseTwo = number * primeNumbers[i];
             if (caseTwo < n) semiPrimeNumbers.push(caseTwo);
         }
-      });
-      return semiPrimeNumbers.length ? semiPrimeNumbers.sort((a,b) => a-b) : 0;
+      });*/
+      //return semiPrimeNumbers.length ? semiPrimeNumbers.sort((a,b) => a-b) : 0;
+      return semiPrimeNumbers.length ? semiPrimeNumbers : 0;
   }
   
-  console.log(semiPrimeCount(20));
+  console.log(semiPrimeCount(70));
