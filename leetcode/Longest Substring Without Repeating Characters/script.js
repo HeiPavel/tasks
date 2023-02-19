6af59e79ee26a7e5c3a61f5e65348ecb19d7f58e
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2023-02-20 01:43:56 +0200

    Longest substring solved but solution are slow-_-

--- leetcode/Longest Substring Without Repeating Characters/script.js
+++ leetcode/Longest Substring Without Repeating Characters/script.js
@@ -1,34 +1,24 @@
 const lengthOfLongestSubstring = s => {
-    let string = s;
     let res = '';
-    let check = true;
     let i = 0;
+    let j = 0;
     let current = '';
-    while (check && i < string.length) {
-        check = false;
-        console.log(`cur: ${current}`, `char: ${s[i]}`);
-        if (!current.includes(string[i])) {
-            current += string[i];
+    while (i < s.length) {
+        if (!current.includes(s[i])) {
+            current += s[i];
             i++;
-            check = true;
         } else {
-            if (current.length > res.length) {
-                res = current;
-                current = '';
-                check = true;
-                i = string.indexOf(string[i]) ? string.indexOf(string[i]) : 1;
-                string = string.slice(i, s.length);
-            } else {
-                current = '';
-                i++;
-                check = true;
-            }
+            if (current.length > res.length) res = current;
+            current = '';
+            j = s.indexOf(s[i]);
+            s = j ? s.slice(j, s.length) : s.slice(j + 1, s.length);
+            i = 0;
         }
     }
     if (current.length > res.length) res = current;
     return res;
 };
 
-const string = 'anviaj';
+const string = 'dvdf';
 
 console.log(lengthOfLongestSubstring(string));
