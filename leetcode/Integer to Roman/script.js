d32dfbb8b91646eeecd8f99a0e4da9ebd0494ab9
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2023-02-23 03:55:24 +0200

    Integer to roman shorter solution

--- leetcode/Integer to Roman/script.js
+++ leetcode/Integer to Roman/script.js
@@ -96,35 +96,36 @@ const intToRoman = (num) => {
     return lowerThan4000(num);
 };
 
-console.log(intToRoman(994));
+console.log(intToRoman(1994));
 
-/*const ver2 = num => {
-    const res = '';
-    let rest = num;
+const ver2 = num => {
+    let res = '';
+    let rest = 0;
     const symbolObj = {
-        1: 'I',
-        2: 'II',
-        3: 'III',
-        4: 'IV',
-        5: 'V',
-        6: 'VI',
-        7: 'VII',
-        8: 'VIII',
-        9: 'IX',
-        10: 'X',
-        40: 'XL',
-        50: 'L',
-        90: 'XC',
-        100: 'C',
-        400: 'CD',
-        500: 'D',
-        900: 'CM',
-        1000: 'M'
+        1: 'I',2: 'II',3: 'III',4: 'IV',5: 'V',6: 'VI',7: 'VII',8: 'VIII',9: 'IX',10: 'X',
+        20: 'XX', 30: 'XXX', 40: 'XL',50: 'L',60: 'LX',70:'LXX',80:'LXXX',90: 'XC',100: 'C',
+        200: 'CC',300: 'CCC',400: 'CD',500: 'D',600: 'DC',700: 'DCC', 800: 'DCCC',900: 'CM',1000: 'M'
     }
     if (symbolObj[num]) return symbolObj[num];
     let decDigit = 0;
-    if (num < 50) {
+    if (num < 100) {
         decDigit = num % 10;
-        while (rest > 0)
+        return symbolObj[num - decDigit] + symbolObj[decDigit];
     }
-}*/
+    if (num < 1000) {
+        decDigit = num % 100;
+        rest = num - decDigit;
+        return symbolObj[rest] + ver2(decDigit);
+    }
+    if (num < 4000) {
+        decDigit = num % 1000;
+        rest = num - decDigit;
+        while (rest > 0) {
+            rest -= 1000;
+            res += 'M';
+        }
+        return decDigit ? res + ver2(decDigit) : res;
+    }
+}
+
+console.log(ver2(1994));
